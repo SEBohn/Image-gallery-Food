7f73481a84f644fd57a56082adb32fba8b077cae
Jinnis <bohnsarahe@gmail.com>
2025-02-04 16:54:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+//JS for the panels to move from active to inactive
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
